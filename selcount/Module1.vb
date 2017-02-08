dfcdd9e7c81a88284e74338c7400ac17a0d603dc
Gijsie88 <gijsie88@gmail.com>
2017-02-08 16:57:08 +0100

    Selcount
    Sample project final version

---
+++ selcount/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Runtime.InteropServices
+
+Module Module1
+
+    Sub Main()
+        Dim oApp As SolidEdgeFramework.Application
+        Dim oDoc As SolidEdgeDraft.DraftDocument
+        Dim oSel As SolidEdgeFramework.SelectSet
+        Dim iCount As Integer
+
+        oApp = Marshal.GetActiveObject("SolidEdge.Application")
+        oDoc = oApp.ActiveDocument
+        oSel = oDoc.SelectSet
+        iCount = oSel.Count.ToString()
+
+        oApp.StatusBar = iCount.ToString() + " object(s) selected."
+
+    End Sub
+
+End Module
