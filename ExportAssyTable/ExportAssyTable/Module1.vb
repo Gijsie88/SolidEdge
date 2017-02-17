b5a5a807f4898bdf2c116cb2ae774cec9f3580a5
Gijsie88 <gijsie88@gmail.com>
2017-02-17 10:25:39 +0100

    ReportBom
    Added ReportBom.
    Status: .exe does not run

---
+++ ExportAssyTable/ExportAssyTable/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Runtime.InteropServices
+
+
+Module Module1
+
+    Sub Main()
+        Dim oApp As SolidEdgeFramework.Application
+        Dim oDoc As SolidEdgeAssembly.AssemblyDocument
+        Dim oSel As SolidEdgeFramework.SelectSet
+        Dim iCount As Integer
+
+        oApp = Marshal.GetActiveObject("SolidEdge.Application")
+        oDoc = oApp.ActiveDocument
+        SolidEdgeConstants.AssemblyCommandConstants.AssemblyAssemblyToolsReports
+
+
+        MsgBox(oDoc.Name)
+
+    End Sub
+
+End Module
---
+++ ExportAssyTable/ExportAssyTable/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ExportAssyTable")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ExportAssyTable")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0e42e3b6-66b0-4b3a-9b3c-8bced0a156c1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
